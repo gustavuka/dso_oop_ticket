40819f289c1e5e3b8c8f7879b48b66325d8291e8
Douglas <38021959+DouglasSantos94@users.noreply.github.com>
2019-10-08 10:31:57 -0300

    Update controlador_evento.py

--- MVC/controle/controlador_evento.py
+++ MVC/controle/controlador_evento.py
@@ -1,4 +1,7 @@
 from local import Local
+from evento import Evento
+from datetime import datetime
+
 
 class ControladorEvento:
 
@@ -6,6 +9,38 @@ class ControladorEvento:
         self.__eventos = []
         self.__locais = []
 
+    @property
+    def eventos(self):
+        return self.__eventos
+    
+    @property
+    def locais(self):
+        return self.__locais
+
     def cadastrar_local(self, local: Local):
         if local not in self.locais:
             self.__locais.append(local)
+
+    def cadastrar_evento(self, evento: Evento):
+        if evento not in self.eventos and isinstance(evento, Evento):
+            self.eventos.append(evento)
+
+    def alterar_local_evento(self, evento: Evento, novo_local: Local):
+        if evento in self.eventos:
+            evento.local = novo_local
+
+    def altera_categoria_evento(self, evento: Evento, nova_categoria: str):
+        if evento in self.eventos:
+            evento.categoria = nova_categoria
+
+    def altera_titulo_evento(self, evento: Evento, novo_titulo: str):
+        if evento in self.eventos:
+            evento.titulo = novo_titulo
+
+    def altera_valor_evento(self, evento: Evento, novo_valor: float):
+        if evento in self.eventos:
+            evento.valor = novo_valor
+
+    def altera_data_evento(self, evento: Evento, nova_data: datetime):
+        if evento in self.eventos:
+            evento.data = nova_data
