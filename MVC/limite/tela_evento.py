7e8012ff41da03ff5cd79891696c33f99361f421
Douglas <38021959+DouglasSantos94@users.noreply.github.com>
2019-10-09 09:14:21 -0300

    Update tela_evento.py

--- MVC/limite/tela_evento.py
+++ MVC/limite/tela_evento.py
@@ -1,4 +1,32 @@
 from controlador_evento import ControladorEvento
 
 
-class TelaEvento(ControladorEvento):
+class TelaEvento:
+
+    def __init__(self, controlador: ControladorEvento):
+        self.__controlador = controlador
+    
+    @property
+    def controlador(self):
+        return self.__controlador
+    
+    def ler_codigo_evento(self):
+        pass
+    
+    def cadastrar_evento(self):
+        pass
+    
+    def alterar_local_evento(self, codigo_evento: str):
+        pass
+    
+    def alterar_categoria_evento(self, codigo_evento: str):
+        pass
+    
+    def alterar_titulo_evento(self, codigo_evento: str):
+        pass
+    
+    def alterar_valor_evento(self, codigo_evento: str):
+        pass
+    
+    def alterar_data_evento(self, codigo_evento: str):
+        pass
